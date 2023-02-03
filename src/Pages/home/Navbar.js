e28a507964abfdcee6eefba424e7e340445b8f30
Rahul <rahulsinghrawat9797@gmail.com>
2023-02-03 18:33:31 +0530

    Navbar - frontend styles done

--- src/Pages/home/Navbar.js
+++ src/Pages/home/Navbar.js
@@ -3,16 +3,23 @@ import "../../css/Pages/home/navbar.css";
 
 // section (A) page links - about site
 const hamburger_pages = [
-  {name: "internships", link: ""},
-  {name: "jobs", link: ""},
-  {name: "contact us", link: ""},
+  { name: "internships", link: "" },
+  { name: "jobs", link: "" },
+  { name: "consultants", link: "" },
+  { name: "contact us", link: "" },
 ]
 
 // section (B) page links - user login/register
 const hamburger_user = [
-  {name: "register - as job seeker", link: ""},
-  {name: "register - as employer", link: ""},
-  {name: "login", link: ""},
+  { name: "register - as job seeker", link: "" },
+  { name: "register - as consultant", link: "" },
+  { name: "login", link: "" },
+]
+
+// section (B) page links - user login/register
+const register_options = [
+  { name: "As a student", link: "" },
+  { name: "As a consultant", link: "" }
 ]
 
 function Navbar() {
@@ -31,21 +38,17 @@ function Navbar() {
 
   return (
     <header className="navbar">
-
       <div className="holder">
-        
         <span className="navbar__hamburger">
-          <i class="uil uil-bars"></i>
-
+          <i className="uil uil-bars"></i>
           <div className="black-screen"></div>
-
           <div className="navbar__hamburger__menu">
-            {hamburger_pages.map((page) => {
-              return <li className="navbar__hamburger__menu--link">{page.name}</li>
+            {hamburger_pages.map((page, i) => {
+              return <li key={i} className="navbar__hamburger__menu--link">{page.name}</li>
             })}
             <hr />
-            {hamburger_user.map((page) => {
-              return <li className="navbar__hamburger__menu--link">{page.name}</li>
+            {hamburger_user.map((page, i) => {
+              return <li key={i} className="navbar__hamburger__menu--link">{page.name}</li>
             })}
           </div>
         </span>
@@ -55,14 +58,23 @@ function Navbar() {
         </span>
 
         <span className="navbar__links">
-          <li to="/" className="navbar__links--link">Jobs <i className="uil uil-angle-down"></i></li>
-          <li to="/" className="navbar__links--link space">Internships <i className="uil uil-angle-down"></i></li>
+          <li to="/" className="navbar__links--link">Jobs</li>
+          <li to="/" className="navbar__links--link space">Internships</li>
+          <li to="/" className="navbar__links--link">Consultants</li>
           <li to="/" className="navbar__links--link">Contact Us</li>
         </span>
 
         <span className="navbar__buttons">
           <button className="navbar__buttons--login">Login</button>
-          <button className="navbar__buttons--register">Register <i className="uil uil-angle-down"></i></button>
+          <button className="navbar__buttons--register">
+            <span>Register</span>
+            <i className="uil uil-angle-down"></i>
+            <div className="navbar__buttons--register-menu">
+              {register_options.map((option, i) => {
+                return <div key={i} className="navbar__buttons--register-option">{option.name}</div>
+              })}
+            </div>
+          </button>
         </span>
       </div>
     </header>

c6e076c863e754ae964a4587a083376e402c19ab
Rahul <rahulsinghrawat9797@gmail.com>
2023-02-05 15:26:54 +0530

    Middle - categories done

--- src/Pages/home/Middle.js
+++ src/Pages/home/Middle.js
@@ -1,5 +1,21 @@
 import React from "react";
 import "../../css/Pages/home/middle.css";
+import {internship_cities} from "./categories-db"
+
+function Categories({title, categories}) {
+  return (
+    <div className="container">
+      <h3>{title}</h3>
+      <div className="container__categories">
+        {categories.map((cat) => {
+          return <div className="container__categories--option">
+            <img src={cat.img} alt="option" />
+            <p>{cat.name}</p>
+          </div>})}
+      </div>
+    </div>
+  )
+}
 
 function Middle() {
   return (
@@ -21,6 +37,40 @@ function Middle() {
         </div>
         <button className="details__searchbox--search">Search</button>
       </div>
+
+      <div className="internship cat-container">
+        <div className="cat-container__header">
+          <span className="cat-container__header--text">
+            <h1>Internships</h1>
+            <p>Apply to 10,000+ internships for free</p>
+          </span>
+
+          <span className="cat-container__header--link">
+            <p>View all internships</p>
+            <i className="uil uil-arrow-right"></i>
+          </span>
+        </div>
+
+        <Categories title={"Popular cities"} categories={internship_cities} />
+        <Categories title={"Popular categories"} categories={internship_cities} />
+      </div>
+
+      {/* <div className="job cat-container">
+        <div className="cat-container__header">
+          <span className="cat-container__header--text">
+            <h1>Jobs</h1>
+            <p>Apply to 10,000+ Jobs for free</p>
+          </span>
+
+          <span className="cat-container__header--link">
+            <p>View all jobs</p>
+            <i className="uil uil-arrow-right"></i>
+          </span>
+        </div>
+
+        <Categories title={"Popular cities"} categories={internship_cities} />
+        <Categories title={"Popular categories"} categories={internship_cities} />
+      </div> */}
 
     </section>
   );
---
+++ src/Pages/home/categories-db.js
@@ -0,0 +1,10 @@
+export const internship_cities = [
+  { name: "Work From Home", img: "/assets/icons/work-from-home.png", link: ""},
+  { name: "Delhi/NCR", img: "/assets/icons/delhi.png", link: ""},
+  { name: "Bangalore", img: "/assets/icons/bangalore.png", link: ""},
+  { name: "Mumbai", img: "/assets/icons/mumbai.png", link: ""},
+  { name: "Hyderabad", img: "/assets/icons/hyderabad.png", link: ""},
+  { name: "Chennai", img: "/assets/icons/chennai.png", link: ""},
+  { name: "Kolkata", img: "/assets/icons/kolkata.png", link: ""},
+  { name: "International", img: "/assets/icons/international.png", link: ""},
+]

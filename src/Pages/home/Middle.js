8cb5b24e0a4d6630c861f94644544e2163932a99
Rahul <rahulsinghrawat9797@gmail.com>
2023-02-05 16:46:59 +0530

    Middle - Categories carousel added

--- src/Pages/home/Middle.js
+++ src/Pages/home/Middle.js
@@ -1,18 +1,51 @@
 import React from "react";
 import "../../css/Pages/home/middle.css";
-import {internship_cities} from "./categories-db"
+import {internship_cities} from "./categories-db";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const sliderOptions = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+}
 
 function Categories({title, categories}) {
+
+  let result = 6
+
+  const width = window.innerWidth;
+
+  if (width <= 340) {
+    result = 1;
+  }
+  else if (width <= 500) {
+    result = 2;
+  }
+  else if (width <= 600) {
+    result = 3;
+  }
+  else if (width <= 800) {
+    result = 4;
+  }
+  else if (width <= 900) {
+    result = 5;
+  }
+  else if (width > 1200) {
+    result = 8;
+  }
+
   return (
     <div className="container">
       <h3>{title}</h3>
-      <div className="container__categories">
-        {categories.map((cat) => {
-          return <div className="container__categories--option">
+      <Slider {...sliderOptions} slidesToShow={result} slidesToScroll={result} className="container__categories">
+        {categories.map((cat, i) => {
+          return <div key={i} className="container__categories--option">
             <img src={cat.img} alt="option" />
             <p>{cat.name}</p>
           </div>})}
-      </div>
+      </Slider>
     </div>
   )
 }
@@ -52,10 +85,10 @@ function Middle() {
         </div>
 
         <Categories title={"Popular cities"} categories={internship_cities} />
-        <Categories title={"Popular categories"} categories={internship_cities} />
+        <Categories title={"Popular cities"} categories={internship_cities} />
       </div>
 
-      {/* <div className="job cat-container">
+      <div className="job cat-container">
         <div className="cat-container__header">
           <span className="cat-container__header--text">
             <h1>Jobs</h1>
@@ -70,7 +103,8 @@ function Middle() {
 
         <Categories title={"Popular cities"} categories={internship_cities} />
         <Categories title={"Popular categories"} categories={internship_cities} />
-      </div> */}
+      </div>
+      
 
     </section>
   );
